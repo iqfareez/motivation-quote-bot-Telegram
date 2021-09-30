285d2573f8d540adde9885b6bc875ae8bac6d40c
iqfareez <iqmal3@outlook.com>
2021-09-30 19:19:13 +0800

    :lock: Revoke bot token
    deployment streamlined

--- bot.py
+++ bot.py
@@ -2,18 +2,17 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 import random
-from CONSTANTS import *
-
 """ forked from liuhh02 https://medium.com/@liuhh02 """
 
 PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = TELEGRAM_BOT_TOKEN
+BOT_TOKEN = os.environ['TELE_BOT_TOKEN']
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -37,56 +36,76 @@ def help(update, context):
 
 def getstudymotiv(update, context):
     # content by https://www.daniel-wong.com/2015/10/05/study-motivation-quotes/
-    quotes = ["Believe you can and you’re halfway there.", "You have to expect things of yourself before you can do them.",
-              "It always seems impossible until it’s done.", "Don’t let what you cannot do interfere with what you can do. – John Wooden",
-              "Start where you are. Use what you have. Do what you can. – Arthur Ashe", "Successful and unsuccessful people do not vary greatly in their abilities.",
-              "They vary in their desires to reach their potential. – John Maxwell", "The secret of success is to do the common things uncommonly well. – John D. Rockefeller",
-              "Good things come to people who wait, but better things come to those who go out and get them.", "Strive for progress, not perfection.",
-              "I find that the harder I work, the more luck I seem to have. – Thomas Jefferson", "Success is the sum of small efforts, repeated day in and day out. – Robert Collier",
-              "Don’t wish it were easier; wish you were better. – Jim Rohn",
-              "I don’t regret the things I’ve done. I regret the things I didn’t do when I had the chance.",
-              "There are two kinds of people in this world: those who want to get things done and those who don’t want to make mistakes. – John Maxwell",
-              "The secret to getting ahead is getting started.", "You don’t have to be great to start, but you have to start to be great.",
-              "The expert in everything was once a beginner.", "There are no shortcuts to any place worth going. – Beverly Sills",
-              "Push yourself, because no one else is going to do it for you.", "Some people dream of accomplishing great things. Others stay awake and make it happen.",
-              "There is no substitute for hard work. – Thomas Edison", "The difference between ordinary and extraordinary is that little “extra.”",
-              "You don’t always get what you wish for; you get what you work for.", "It’s not about how bad you want it. It’s about how hard you’re willing to work for it.",
-              "The only place where success comes before work is in the dictionary. – Vidal Sassoon", "There are no traffic jams on the extra mile. – Zig Ziglar",
-              "If people only knew how hard I’ve worked to gain my mastery, it wouldn’t seem so wonderful at all. – Michelangelo",
-              "“All our dreams can come true, if we have the courage to pursue them.” – Walt Disney.",
-              "Success is not final, failure is not fatal: it is the courage to continue that counts.",
-              "If it’s important to you, you’ll find a way. If not, you’ll find an excuse.", "Don’t say you don’t have enough time. You have exactly the same number of hours per day that were given to Helen Keller, Pasteur, Michelangelo, Mother Teresea, Leonardo da Vinci, Thomas Jefferson, and Albert Einstein. – H. Jackson Brown Jr.",
-              "Challenges are what make life interesting. Overcoming them is what makes life meaningful. – Joshua J. Marine",
-              "Life has two rules: 1) Never quit. 2) Always remember Rule #1.", "I’ve failed over and over and over again in my life. And that is why I succeed. – Michael Jordan",
-              "I don’t measure a man’s success by how high he climbs, but how high he bounces when he hits the bottom. – George S. Patton",
-              "If you’re going through hell, keep going. – Winston Churchill", "Don’t let your victories go to your head, or your failures go to your heart.",
-              "Failure is the opportunity to begin again more intelligently. – Henry Ford",
-              "Believe you can and you're halfway there.",
-              "Strive for progress, not perfection",
-              "Dream don't work unless you do",
-              "Never let your fear decide your future.",
-              "You don’t drown by falling in the water; you drown by staying there. – Ed Cole", "The difference between a stumbling block and a stepping-stone is how high you raise your foot.",
-              "The pain you feel today is the strength you will feel tomorrow. For every challenge encountered there is opportunity for growth.",
-              "“Be like a diamond, precious and rare, not like a stone, found everywhere.” Anonymous",
-              "It’s not going to be easy, but it’s going to be worth it."]
+    quotes = [
+        "Believe you can and you’re halfway there.",
+        "You have to expect things of yourself before you can do them.",
+        "It always seems impossible until it’s done.",
+        "Don’t let what you cannot do interfere with what you can do. – John Wooden",
+        "Start where you are. Use what you have. Do what you can. – Arthur Ashe",
+        "Successful and unsuccessful people do not vary greatly in their abilities.",
+        "They vary in their desires to reach their potential. – John Maxwell",
+        "The secret of success is to do the common things uncommonly well. – John D. Rockefeller",
+        "Good things come to people who wait, but better things come to those who go out and get them.",
+        "Strive for progress, not perfection.",
+        "I find that the harder I work, the more luck I seem to have. – Thomas Jefferson",
+        "Success is the sum of small efforts, repeated day in and day out. – Robert Collier",
+        "Don’t wish it were easier; wish you were better. – Jim Rohn",
+        "I don’t regret the things I’ve done. I regret the things I didn’t do when I had the chance.",
+        "There are two kinds of people in this world: those who want to get things done and those who don’t want to make mistakes. – John Maxwell",
+        "The secret to getting ahead is getting started.",
+        "You don’t have to be great to start, but you have to start to be great.",
+        "The expert in everything was once a beginner.",
+        "There are no shortcuts to any place worth going. – Beverly Sills",
+        "Push yourself, because no one else is going to do it for you.",
+        "Some people dream of accomplishing great things. Others stay awake and make it happen.",
+        "There is no substitute for hard work. – Thomas Edison",
+        "The difference between ordinary and extraordinary is that little “extra.”",
+        "You don’t always get what you wish for; you get what you work for.",
+        "It’s not about how bad you want it. It’s about how hard you’re willing to work for it.",
+        "The only place where success comes before work is in the dictionary. – Vidal Sassoon",
+        "There are no traffic jams on the extra mile. – Zig Ziglar",
+        "If people only knew how hard I’ve worked to gain my mastery, it wouldn’t seem so wonderful at all. – Michelangelo",
+        "“All our dreams can come true, if we have the courage to pursue them.” – Walt Disney.",
+        "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+        "If it’s important to you, you’ll find a way. If not, you’ll find an excuse.",
+        "Don’t say you don’t have enough time. You have exactly the same number of hours per day that were given to Helen Keller, Pasteur, Michelangelo, Mother Teresea, Leonardo da Vinci, Thomas Jefferson, and Albert Einstein. – H. Jackson Brown Jr.",
+        "Challenges are what make life interesting. Overcoming them is what makes life meaningful. – Joshua J. Marine",
+        "Life has two rules: 1) Never quit. 2) Always remember Rule #1.",
+        "I’ve failed over and over and over again in my life. And that is why I succeed. – Michael Jordan",
+        "I don’t measure a man’s success by how high he climbs, but how high he bounces when he hits the bottom. – George S. Patton",
+        "If you’re going through hell, keep going. – Winston Churchill",
+        "Don’t let your victories go to your head, or your failures go to your heart.",
+        "Failure is the opportunity to begin again more intelligently. – Henry Ford",
+        "Believe you can and you're halfway there.",
+        "Strive for progress, not perfection",
+        "Dream don't work unless you do",
+        "Never let your fear decide your future.",
+        "You don’t drown by falling in the water; you drown by staying there. – Ed Cole",
+        "The difference between a stumbling block and a stepping-stone is how high you raise your foot.",
+        "The pain you feel today is the strength you will feel tomorrow. For every challenge encountered there is opportunity for growth.",
+        "“Be like a diamond, precious and rare, not like a stone, found everywhere.” Anonymous",
+        "It’s not going to be easy, but it’s going to be worth it."
+    ]
     length = len(quotes)
     randomIndex = random.randint(0, length - 1)
     update.message.reply_text(quotes[randomIndex])
 
 
 def getislamicmotiv(update, context):
-    quotes = ["“If Allah wants to do good to somebody, he afflicts him with trials.” – Sahih Al Bukhari",
-              "Doctors can treat you, but only ALLAH can heal you",
-              "Allah is enough",
-              "When things are too hard to handle, retreat & count your blessings instead.",
-              "Allah’s timing is perfect in every matter. We don’t always understand the wisdom behind it. But we have to learn to trust it.",
-              "Allah knows what is the best for you and when it’s best for you to have it.",
-              "No matter what your physical appearance, when you have kindness in your heart, You’re the most beautiful person in the world. – Mufti Ismail Menk",
-              "“Do not lose hope, nor be sad.” Quran 3:139",
-              "“Call upon Me, I will respond to you.” Quran 40:60",
-              "“Never underestimate the power of Dua (supplication).” Anonymous",
-              "“Allah (God) does not burden a soul beyond that it can bear.” Quran 2:286",
-              "“They plan, and Allah (God) plans. Surely, Allah (God) is the best of planners.” Quran 8:30"]
+    quotes = [
+        "“If Allah wants to do good to somebody, he afflicts him with trials.” – Sahih Al Bukhari",
+        "Doctors can treat you, but only ALLAH can heal you",
+        "Allah is enough",
+        "When things are too hard to handle, retreat & count your blessings instead.",
+        "Allah’s timing is perfect in every matter. We don’t always understand the wisdom behind it. But we have to learn to trust it.",
+        "Allah knows what is the best for you and when it’s best for you to have it.",
+        "No matter what your physical appearance, when you have kindness in your heart, You’re the most beautiful person in the world. – Mufti Ismail Menk",
+        "“Do not lose hope, nor be sad.” Quran 3:139",
+        "“Call upon Me, I will respond to you.” Quran 40:60",
+        "“Never underestimate the power of Dua (supplication).” Anonymous",
+        "“Allah (God) does not burden a soul beyond that it can bear.” Quran 2:286",
+        "“They plan, and Allah (God) plans. Surely, Allah (God) is the best of planners.” Quran 8:30"
+    ]
     length = len(quotes)
     randomIndex = random.randint(0, length - 1)
     update.message.reply_text(quotes[randomIndex])
@@ -96,7 +115,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater(BOT_TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -114,11 +133,9 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook(
-        HEROKU_APP_LINK + TOKEN)
+    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=BOT_TOKEN)
+    updater.bot.setWebhook('https://motivate-us-bot.herokuapp.com/' +
+                           BOT_TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
